00025d2a0c1fa9d5f35779bafa1d6af4fa6d7f60
unknown <yeehawbw@gmail.com>
2020-04-14 17:35:31 -0400

    Get fetch to API working without CORS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  /*
+    https://api.wordassociations.net/associations/v1.0/json/search?
+    apikey=<API key>
+    & text=<word or phrase>
+    & lang=<language>
+    & [type=<result type>]
+    & [limit=<maximum number of results>]
+    & [pos=<parts of speech>]
+    & [indent=<yes or no>]
+  */
+
+  let [resp, setResp] = useState('');
+
+  /*fetch(
+    `https://api.wordassociations.net/associations/v1.0/json/search?
+    apikey=2f06934d-533f-4380-943c-460ce962753e
+    &text=cloudy
+    &lang=en
+    &type=stimulus
+    &limit=10
+    &pos=noun,verb
+    &indent=yes`,
+    {
+      mode: 'no-cors',,
+    }
+  ) */
+
+  useEffect(() => {
+    fetch(`https://api.datamuse.com/words?rel_jja=yellow`, {
+      cache: 'no-cache',
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setResp(data.toString());
+      });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div contentEditable='true' spellCheck='true' className='editor'>
+          {resp}
+        </div>
       </header>
     </div>
   );
