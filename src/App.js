1cf50fa258a6fe368746a7ae48cd89ee8c9e09c5
unknown <yeehawbw@gmail.com>
2020-04-15 11:24:07 -0400

    Basic editor styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+/*
+  https://api.wordassociations.net/associations/v1.0/json/search?
+  apikey=<API key>
+  & text=<word or phrase>
+  & lang=<language>
+  & [type=<result type>]
+  & [limit=<maximum number of results>]
+  & [pos=<parts of speech>]
+  & [indent=<yes or no>]
+*/
+
+/*fetch(
+   `https://api.wordassociations.net/associations/v1.0/json/search?
+   apikey=2f06934d-533f-4380-943c-460ce962753e
+   &text=cloudy
+   &lang=en
+   &type=stimulus
+   &limit=10
+   &pos=noun,verb
+   &indent=yes`,
+   {
+     mode: 'no-cors',,
+   }
+ ) */
+
 function App() {
-  /*
-    https://api.wordassociations.net/associations/v1.0/json/search?
-    apikey=<API key>
-    & text=<word or phrase>
-    & lang=<language>
-    & [type=<result type>]
-    & [limit=<maximum number of results>]
-    & [pos=<parts of speech>]
-    & [indent=<yes or no>]
-  */
-
   let [resp, setResp] = useState('');
-
-  /*fetch(
-    `https://api.wordassociations.net/associations/v1.0/json/search?
-    apikey=2f06934d-533f-4380-943c-460ce962753e
-    &text=cloudy
-    &lang=en
-    &type=stimulus
-    &limit=10
-    &pos=noun,verb
-    &indent=yes`,
-    {
-      mode: 'no-cors',,
-    }
-  ) */
 
   useEffect(() => {
     fetch(`https://api.datamuse.com/words?rel_jja=yellow`, {
@@ -38,13 +38,14 @@ function App() {
       })
       .then((data) => {
         console.log(data);
-        setResp(data.toString());
+        setResp(JSON.stringify(data));
       });
   });
 
   return (
     <div className='App'>
       <header className='App-header'>
+        <div className='top-bar'>A N T I - A D J E C T I V E</div>
         <div contentEditable='true' spellCheck='true' className='editor'>
           {resp}
         </div>
