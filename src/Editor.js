beb18777fd8a7c752b8480bb190b52df1fd8f065
unknown <yeehawbw@gmail.com>
2020-04-18 11:03:19 -0400

    Integrate compromise and set editable html

--- src/Editor.js
+++ src/Editor.js
@@ -3,7 +3,30 @@ import debounce from './debounce';
 import nlp from 'compromise';
 
 export default function Editor() {
-  let sent = 'Hello. I am me. I am not feeling bad.';
+  let sent =
+    'I walked briskly, swiftly to the store. A happy bird saw me. It was happy.';
+
+  let words = nlp(sent);
+  words.match('#Verb #Adverb+').forEach((term) => {
+    term.prepend('<span>');
+    term.append('</span>');
+  });
+  words.match('#Adverb+ #Verb').forEach((term) => {
+    term.prepend('<span>');
+    term.append('</span>');
+  });
+  words.match('#Adjective+ #Noun').forEach((term) => {
+    term.prepend('<span>');
+    term.append('</span>');
+  });
+  words.match('#Noun #Adjective+').forEach((term) => {
+    term.prepend('<span>');
+    term.append('</span>');
+  });
+  words.match('#Adjective').forEach((term) => {
+    term.prepend('<span>');
+    term.append('</span>');
+  });
 
   const [text, setText] = useState('');
 
@@ -13,7 +36,7 @@ export default function Editor() {
 
   const debouncedChangeFunction = debounce(changeFunction, 2000);
 
-  const persistChangeFunction = function (e) {
+  const persistingChangeFunction = function (e) {
     debouncedChangeFunction(e.nativeEvent);
   };
 
@@ -22,8 +45,9 @@ export default function Editor() {
       contentEditable='true'
       spellCheck='true'
       className='editor'
-      onKeyUp={persistChangeFunction}>
-      {sent}
+      onKeyUp={persistingChangeFunction}>
+      <span dangerouslySetInnerHTML={{ __html: words.text() }}></span>
+      <span className='adjective'>"The other span"</span>
     </div>
   );
 }
