ea657c196123ff81fca2a30c4ed3c3534301880d
unknown <yeehawbw@gmail.com>
2020-04-20 17:57:04 -0400

    Change token mapping to split+map

--- src/Editor.js
+++ src/Editor.js
@@ -4,35 +4,18 @@ import nlp from 'compromise';
 
 export default function Editor() {
   let sent =
-    'I walked briskly, swiftly to the store. The happy bird saw me. It was happy.';
+    'I walked briskly, swiftly to the store. The happy bird saw me. It was happy and yellow. The end. The end again.';
 
   let words = nlp(sent);
 
-  let terms = [];
-
-  words.match('#Verb #Adverb+').forEach((term) => {
-    terms.push(term.text());
-    term.prepend(`<span class="adjective" dataset-index=${terms.length - 1}>`);
-    term.append('</span>');
-  });
-  words.match('#Adverb+ #Verb').forEach((term) => {
-    terms.push(term.text());
-    term.prepend(`<span class="adjective" dataset-index=${terms.length - 1}>`);
-    term.append('</span>');
-  });
-  words.match('#Adjective+ #Noun').forEach((term) => {
-    terms.push(term.text());
-    term.prepend(`<span class="adjective" dataset-index=${terms.length - 1}>`);
-    term.append('</span>');
-  });
-
-  words.match('#Adjective').forEach((term) => {
-    terms.push(term.text());
-    term.prepend(`<span class="adjective" dataset-index=${terms.length - 1}>`);
-    term.append('</span>');
-  });
-
-  console.log(terms);
+  let terms = words
+    .splitBefore('#Verb #Adverb+')
+    .splitAfter('#Verb #Adverb+')
+    .splitBefore('#Adverb+ #Verb')
+    .splitAfter('#Adverb+ #Verb')
+    .splitBefore('#Adjective+ #Noun')
+    .splitAfter('#Adjective+ #Noun')
+    .out('array');
 
   const [text, setText] = useState('');
 
@@ -53,6 +36,14 @@ export default function Editor() {
       className='editor'
       onKeyUp={persistingChangeFunction}>
       <span dangerouslySetInnerHTML={{ __html: words.text() }}></span>
+      {terms.map((term, index) =>
+        Boolean(nlp(term).match('#Adverb').text()) ||
+        Boolean(nlp(term).match('#Adjective+ #Noun').text()) ? (
+          <span className='adjective'>{`${term} `}</span>
+        ) : (
+          <span>{`${term} `}</span>
+        )
+      )}
     </div>
   );
 }
