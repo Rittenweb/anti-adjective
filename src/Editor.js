c90ea8adfbeff00328b58eb1c022c4d86663b8f9
unknown <yeehawbw@gmail.com>
2020-04-21 15:55:58 -0400

    Get persistent caret selection except w/ new nodes

--- src/Editor.js
+++ src/Editor.js
@@ -3,24 +3,58 @@ import debounce from './debounce';
 import nlp from 'compromise';
 
 export default function Editor() {
-  let sent =
-    'I walked briskly, swiftly to the store. The happy bird saw me. It was happy and yellow. The end. The end again.';
-
-  let words = nlp(sent);
-
-  let terms = words
-    .splitBefore('#Verb #Adverb+')
-    .splitAfter('#Verb #Adverb+')
-    .splitBefore('#Adverb+ #Verb')
-    .splitAfter('#Adverb+ #Verb')
-    .splitBefore('#Adjective+ #Noun')
-    .splitAfter('#Adjective+ #Noun')
-    .out('array');
-
-  const [text, setText] = useState('');
+  const [terms, setTerms] = useState('');
+  const [caretPosition, setCaretPosition] = useState(0);
 
   const changeFunction = function (e) {
-    setText(e.target.innerText);
+    //first: save innerText to localStorage
+    //let range = document.createRange();
+    let sel = document.getSelection();
+    let anchorNodeNumber;
+    const editor = e.target;
+    for (let i = 0; i < editor.childNodes.length; i++) {
+      let currentChildNode = editor.childNodes[i];
+      if (currentChildNode.nodeType !== Node.TEXT_NODE) {
+        currentChildNode = currentChildNode.childNodes[0];
+      }
+      if (currentChildNode === sel.anchorNode) {
+        anchorNodeNumber = i;
+        break;
+      }
+    }
+    console.log('anchor number' + anchorNodeNumber);
+    let offset = sel.anchorOffset;
+    let words = nlp(e.target.innerText);
+    let adjs = words.match('#Adjective');
+    adjs.forEach((el) => {
+      el.prepend('<span class="adjective">');
+      el.append('</span>');
+    });
+    /*
+    let newTerms = words
+      .splitBefore('#Verb #Adverb+')
+      .splitAfter('#Verb #Adverb+')
+      .splitBefore('#Adverb+ #Verb')
+      .splitAfter('#Adverb+ #Verb')
+      .splitBefore('#Adjective+ #Noun')
+      .splitAfter('#Adjective+ #Noun')
+      .out('array');
+    e.target.innerText = '';
+    */
+    setTerms(words.text());
+    /*
+    range.setStart(e.target, 1);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    */
+    let newAnchorNode;
+    if (editor.childNodes[anchorNodeNumber].nodeType !== Node.TEXT_NODE) {
+      newAnchorNode = editor.childNodes[anchorNodeNumber].childNodes[0];
+    } else {
+      newAnchorNode = editor.childNodes[anchorNodeNumber];
+    }
+    sel.setPosition(newAnchorNode, offset);
   };
 
   const debouncedChangeFunction = debounce(changeFunction, 2000);
@@ -34,16 +68,17 @@ export default function Editor() {
       contentEditable='true'
       spellCheck='true'
       className='editor'
-      onKeyUp={persistingChangeFunction}>
-      <span dangerouslySetInnerHTML={{ __html: words.text() }}></span>
-      {terms.map((term, index) =>
-        Boolean(nlp(term).match('#Adverb').text()) ||
+      onKeyUp={persistingChangeFunction}
+      dangerouslySetInnerHTML={{ __html: terms }}>
+      {/* {terms.map((term, index) =>
+        Boolean(nlp(term).match('#Verb #Adverb+').text()) ||
+        Boolean(nlp(term).match('#Adverb+ #Verb').text()) ||
         Boolean(nlp(term).match('#Adjective+ #Noun').text()) ? (
           <span className='adjective'>{`${term} `}</span>
         ) : (
           <span>{`${term} `}</span>
         )
-      )}
+      )} */}
     </div>
   );
 }
