f0c700450c59e7f7ba0fccd997eea26268cdf9c2
unknown <yeehawbw@gmail.com>
2020-04-22 17:34:44 -0400

    Change nlp implementation to html-tagged

--- src/Editor.js
+++ src/Editor.js
@@ -1,45 +1,44 @@
 import React, { useState } from 'react';
 import debounce from './debounce';
 import nlp from 'compromise';
+import nlpo from 'compromise-output';
 
 export default function Editor() {
   const [terms, setTerms] = useState(localStorage.getItem('text') || '');
 
+  const nlpHtml = nlp.extend(nlpo);
+
+  /*
   let escapedHtml = {
     '<span': 'Greeting',
     'class="adjective">': 'Greeting',
     '</span>': 'Greeting',
   };
+  */
 
   const changeFunction = function (e) {
-    //first: save innerText to localStorage
-    //let range = document.createRange();
     let sel = document.getSelection();
-    let anchorNodeNumber;
-    const editor = e.target;
+    const editorNode = e.target.childNodes[0];
     let offset = sel.anchorOffset;
     let currentNodeStart = 0;
 
-    for (let i = 0; i < editor.childNodes.length; i++) {
-      localStorage.setItem('text', e.target.innerText);
+    let anchorNodeNumber;
 
-      let currentChildNode = editor.childNodes[i];
-      if (currentChildNode.nodeType !== Node.TEXT_NODE) {
-        currentChildNode = currentChildNode.childNodes[0];
-      }
-      if (currentChildNode === sel.anchorNode) {
+    for (let i = 0; i < editorNode.childNodes.length; i++) {
+      let currentChildTextNode = editorNode.childNodes[i].childNodes[0];
+
+      if (currentChildTextNode === sel.anchorNode) {
         anchorNodeNumber = i;
         break;
       }
 
-      currentNodeStart += currentChildNode.length;
+      //currentNodeStart += currentChildTextNode.length;
     }
 
-    //get total length of current locationInText
-    //get distance back current node reaches from current locationInText
-    //if current node is cut off,
-
-    let words = nlp(e.target.innerText, escapedHtml);
+    let words = nlpHtml(e.target.innerText);
+    let withMatches = words.html({
+      '#Adjective': `adjective`,
+    });
     let adjs = words.match('#Adjective');
 
     const textAddedValue = 31;
@@ -47,24 +46,29 @@ export default function Editor() {
 
     let insertedIntoCurrentNode = 0;
 
-    adjs.forEach((el) => {
-      let elIndex =
-        el.out('offset')[0].offset.start - textAddedNum * textAddedValue;
-      let elLength = el.out('offset')[0].offset.length;
+    /*
+    adjs.forEach((match) => {
+      let matchIndex =
+        match.out('offset')[0].offset.start - textAddedNum * textAddedValue;
+      let matchLength = match.out('offset')[0].offset.length;
 
-      if (elIndex > currentNodeStart && elIndex < currentNodeStart + offset) {
+      if (
+        matchIndex > currentNodeStart &&
+        matchIndex < currentNodeStart + offset
+      ) {
         anchorNodeNumber += 2;
-        offset = currentNodeStart + offset - elIndex - elLength;
-        currentNodeStart = elIndex + elLength; //length of </span>
+        offset = currentNodeStart + offset - matchIndex - matchLength;
+        currentNodeStart = matchIndex + matchLength;
         insertedIntoCurrentNode++;
       }
 
       textAddedNum++;
-      el.prepend('<span class="adjective">');
-      el.append('</span>');
+      match.prepend('<span class="adjective">');
+      match.append('</span>');
     });
+    */
 
-    offset += (insertedIntoCurrentNode - 1) * 2; //not sure why it works but it does...
+    //offset += (insertedIntoCurrentNode - 1) * 2; //not sure why it works but it does...
 
     /*
     let newTerms = words
@@ -77,23 +81,49 @@ export default function Editor() {
       .out('array');
     e.target.innerText = '';
     */
-    setTerms(words.text());
 
-    let newAnchorNode;
-    if (editor.hasChildNodes()) {
-      if (editor.childNodes[anchorNodeNumber].nodeType !== Node.TEXT_NODE) {
-        newAnchorNode = editor.childNodes[anchorNodeNumber].childNodes[0];
-      } else {
-        newAnchorNode = editor.childNodes[anchorNodeNumber];
-      }
-    }
-    sel.setPosition(newAnchorNode, offset);
+    localStorage.setItem('text', withMatches);
+    setTerms(withMatches);
+
+    console.log(editorNode.childNodes[anchorNodeNumber]);
+    console.log(editorNode.childNodes[anchorNodeNumber].childNodes[0]);
+
+    let targetTextNode = editorNode.childNodes[anchorNodeNumber].childNodes[0];
+    sel.setPosition(targetTextNode, targetTextNode.length - 1);
   };
 
   const debouncedChangeFunction = debounce(changeFunction, 2000);
 
   const persistingChangeFunction = function (e) {
-    debouncedChangeFunction(e.nativeEvent);
+    if (
+      e.key === 'ArrowLeft' ||
+      e.key === 'ArrowRight' ||
+      e.key === 'ArrowUp' ||
+      e.key === 'ArrowDown' ||
+      e.key === 'Alt'
+    ) {
+      return;
+    } else {
+      debouncedChangeFunction(e.nativeEvent);
+    }
+  };
+
+  const specialCommandsFunction = function (e) {
+    /*
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      document.execCommand('insertHTML', false, '<br/>');
+    } else if (e.key === 'Alt') {
+      //do real stuff.
+    }
+    */
+  };
+
+  const pastePlainText = function (e) {
+    e.preventDefault();
+
+    const text = e.clipboardData.getData('text/plain');
+    document.execCommand('insertHTML', false, text);
   };
 
   return (
@@ -102,6 +132,8 @@ export default function Editor() {
       spellCheck='true'
       className='editor'
       onKeyUp={persistingChangeFunction}
+      onKeyPress={specialCommandsFunction}
+      onPaste={pastePlainText}
       dangerouslySetInnerHTML={{ __html: terms }}>
       {/* {terms.map((term, index) =>
         Boolean(nlp(term).match('#Verb #Adverb+').text()) ||
