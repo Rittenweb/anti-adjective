438bd847faeb7297f3cf634cef55f454e8230341
unknown <yeehawbw@gmail.com>
2020-04-30 16:30:21 -0400

    Fix mode-name highlighting

--- src/ModeToggler.js
+++ src/ModeToggler.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function ModeToggler(props) {
   return (
     <div className='toggle-container'>
-      <span className={props.toggleMode ? 'off' : 'on'}>(adv+vb)/(adj+n)</span>
+      <span className={props.mode ? 'off' : 'on'}>(adv+vb)/(adj+n)</span>
       <div className='toggle' onClick={props.handler}>
         mode
       </div>
-      <span className={props.toggleMode ? 'on' : 'off'}>just (adj)</span>
+      <span className={props.mode ? 'on' : 'off'}>just (adj)</span>
     </div>
   );
 }
